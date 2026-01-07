8c2abe79d5d180a4ed55e293dea600da582a99f3
00Bhuwan <9843lalitpur@gmail.com>
2026-01-07 12:44:20 +0545

    final config

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 Scss(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_tut.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 db = SQLAlchemy(app)
 
 # Data CLass ~ row of data
@@ -17,6 +18,9 @@ class MyTask(db.Model):
 
     def __repr__(self) -> str:
         return f"Task {self.id}"
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -64,7 +68,4 @@ def update(id: int):
 
 
 if __name__ == "__main__":
-    with app.app_context():
-       db.create_all()
-
     app.run(debug=True)
