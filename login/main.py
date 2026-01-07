9efcc844ffe876164c3c3441fc2501646907c027
00Bhuwan <9843lalitpur@gmail.com>
2026-01-07 16:21:37 +0545

    new-branch-created

---
+++ login/main.py
@@ -0,0 +1,70 @@
+from flask import Flask, redirect, render_template, request, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+# Configure Database Model
+app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# Database Model
+class User(db.Model):
+    # class variable
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+# routes
+@app.get("/")
+def home():
+    if "username" in session:
+        return redirect(url_for('dashboard'))
+    return render_template("index.html")
+
+# login
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        session['username'] = username
+        return  redirect(url_for("dashboard"))
+    else:
+        return redirect("index.html")
+
+# Register
+@app.route("/register", methods=["POST"]):
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return render_template("index.html", error="User already registered!!")
+    else:
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+
+# Dashboard
+
+
+# logout
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
